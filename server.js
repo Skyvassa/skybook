8696d8323b237930dff28d8c61a706503cf9986e
Skyler Vassallo <Skyler_Vassallo@glic.com>
2021-05-14 13:31:39 -0400

    initial controller / route structure in place

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.render('index.ejs');
+})
+
 app.listen(3000, (req,res) => {
     console.log("Functional on Port 3000!")
 })
