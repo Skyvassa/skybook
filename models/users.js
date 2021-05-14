5f909fcf119aff2d03a8fc0fe305fcbefc4c9d11
Skyler Vassallo <Skyler_Vassallo@glic.com>
2021-05-14 15:34:59 -0400

    Created Views Folders and info

--- models/users.js
+++ models/users.js
@@ -3,7 +3,7 @@ const users = [
         username: "Skyguy",
         email: "Skyguy@guy.com",
         password: "password123",
-        profile: "Lorem ipsum dolor sit amet amet olor sit.",
+        aboutMe: "Lorem ipsum dolor sit amet amet olor sit.",
         img: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3c/Chris_Hemsworth_as_Thor.jpg/220px-Chris_Hemsworth_as_Thor.jpg",
         uuid: 12345
     }
